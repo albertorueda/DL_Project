15b24636acaa409beea8614cd1b7a5d5f3268d73
LaylaDoumont <layla.doumont@student.uclouvain.be>
2025-12-05 11:44:25 +0100

    docstrings models file

--- modules/models.py
+++ modules/models.py
@@ -7,7 +7,20 @@ output_size = 2
 
 ### GRU for sequence-to-sequence 
 class GRUModel(nn.Module):
-    def __init__(self, input_size, embed_size, hidden_size, output_size, num_layers=1, dropout=0.0, first_linear=True):
+    '''
+    A GRU-based sequence-to-sequence model for predicting future positions based on input sequences.
+    Args:
+        input_size (int): The number of expected features in the input (default is 5).
+        embed_size (int): The size of the embedding layer (default is 64).
+        hidden_size (int): The number of features in the hidden state of the GRU (default is 256).
+        output_size (int): The number of expected features in the output (default is 2).
+        num_layers (int): Number of hidden layers in the GRU (default is 1).
+        dropout (float): Dropout probability for the GRU layers (default is 0.1).
+        first_linear (bool): Whether to include a linear embedding layer before the GRU (default is True).
+    Returns:
+        torch.Tensor: The output tensor containing predicted positions with shape (batch_size, seq_length, output_size).
+    '''
+    def __init__(self, input_size = 5, embed_size = 64, hidden_size = 256, output_size = 2, num_layers=1, dropout=0.1, first_linear=True):
         super().__init__()
         self.first_linear = first_linear
 
@@ -51,7 +64,21 @@ class GRUModel(nn.Module):
 ### LSTM for sequence-to-sequence
 
 class LSTMModel(nn.Module):
-    def __init__(self, input_size, embed_size, hidden_size, output_size, num_layers=1, dropout=0.0, first_linear=True):
+    '''
+    A LSTM-based sequence-to-sequence model for predicting future positions based on input sequences.
+    Args:
+        input_size (int): The number of expected features in the input (default is 5).
+        embed_size (int): The size of the embedding layer (default is 64).
+        hidden_size (int): The number of features in the hidden state of the LSTM (default is 256).
+        output_size (int): The number of expected features in the output (default is 2).
+        num_layers (int): Number of hidden layers in the LSTM (default is 1).
+        dropout (float): Dropout probability for the LSTM layers (default is 0.0).
+        first_linear (bool): Whether to include a linear embedding layer before the LSTM (default is True).
+    Returns:
+        torch.Tensor: The output tensor containing predicted positions with shape (batch_size, seq_length, output_size).
+
+    '''
+    def __init__(self, input_size = 5, embed_size = 64, hidden_size = 256, output_size = 2, num_layers=1, dropout=0.0, first_linear=True):
         super().__init__()
         self.first_linear = first_linear
 
@@ -94,31 +121,3 @@ class LSTMModel(nn.Module):
         return out
 
 
-## we will not be using this for now :
-class GRUSeq2VecModel(nn.Module):
-    def __init__(self, input_size, embed_size, hidden_size, output_size, num_layers=1, dropout=0.0):
-        super().__init__()
-
-        # Map input from size 4 (input_size) to size 64 (embed_size)
-        self.embedding = nn.Linear(input_size, embed_size)
-
-        # GRU works in 64-dim space 
-        self.gru = nn.GRU(
-            embed_size,
-            hidden_size,
-            num_layers,
-            bias=True,
-            batch_first=True,
-            dropout = dropout ,
-            bidirectional=False
-        )
-
-        # Map GRU final hidden state from size 64 (hidden_size) to size 2 (output_size)
-        self.fc_out = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        x = self.embedding(x)        # (batch, seq, 64)
-        out, hidden = self.gru(x)    # out: (batch, seq, 64), hidden: (num_layers, batch, 64)
-        out = self.fc_out(hidden[-1]) # Take the last layer's hidden state and map to output size
-
-        return out
